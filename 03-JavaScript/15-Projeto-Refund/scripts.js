d99b82954a840a58b6dbebb3f0f067b4da7f3c25
Davi <davi.rferreira@gmail.com>
2025-08-14 08:26:53 -0300

    Refund - Formatando e limitando os valores digitados no input de valor da despesa

--- 03-JavaScript/15-Projeto-Refund/scripts.js
+++ 03-JavaScript/15-Projeto-Refund/scripts.js
@@ -1,1 +1,20 @@
-console.log("Teste");
+// selecionando os elementos do formulário
+const amount = document.querySelector("input#amount");
+
+
+
+// capturando evento de input para formatar o valor
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+  value = Number(value) / 100;
+  amount.value = formatCurrencyBRL(value);
+};
+
+// formatando o valor para reais brasileiros
+function formatCurrencyBRL(value) {
+  value = Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
