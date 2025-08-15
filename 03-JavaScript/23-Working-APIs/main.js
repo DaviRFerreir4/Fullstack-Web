06774e3dd957934324e9a37e021a2dea82555270
Davi <davi.rferreira@gmail.com>
2025-08-15 11:31:10 -0300

    Estudos sobre Working APIs em JavaScript - Fetch com POST

--- 03-JavaScript/23-Working-APIs/main.js
+++ 03-JavaScript/23-Working-APIs/main.js
@@ -4,12 +4,12 @@
 .then((data) => console.log(data))));*/
 
 // Utilizando o async e await
-/*async function fetchProducts() {
+async function fetchProducts() {
   const response = await fetch("http://localhost:3333/products");
   const data = await response.json();
   console.log(data);
 }
-fetchProducts();*/
+// fetchProducts();
 
 // Passando parâmetros na requisição
 async function fetchProductById(id) {
@@ -17,4 +17,28 @@ async function fetchProductById(id) {
   const data = await response.json();
   console.log(data);
 }
-fetchProductById(2);
+// fetchProductById(2);
+
+// Fetch com post
+const productName = document.querySelector("input#name");
+const productPrice = document.querySelector("input#price");
+const form = document.querySelector("form");
+const button = document.querySelector("button[type='button']");
+
+button.addEventListener("click", async () => {
+  await fetchProducts();
+});
+
+form.addEventListener("submit", async (event) => {
+  await fetch("http://localhost:3333/products", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id: new Date().getTime().toString(),
+      name: productName.value,
+      price: productPrice.value,
+    })
+  });
+});
