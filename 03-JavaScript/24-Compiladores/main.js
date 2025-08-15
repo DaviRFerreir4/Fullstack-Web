fa1c42d0bc188ee8b1595af58807e65d7e9f60e7
Davi <davi.rferreira@gmail.com>
2025-08-15 13:37:23 -0300

    Estudos sobre compiladores em JavaScript - Utilizando o arquivo compilado na aplicação e demonstrando que é necessário recompilar alterações

--- 03-JavaScript/24-Compiladores/main.js
+++ 03-JavaScript/24-Compiladores/main.js
@@ -1,9 +1,14 @@
 class User {
-  constructor({ email }){
+  constructor({ email }) {
     this.email = email;
+  }
+  sendMessage() {
+    console.log("Mensagem enviada para:", this.email); // se quiser que reflita no código, devo recompilar o arquivo com o script criado ("npm run build")
   }
 }
 
 let user = new User({ email: "davi@email.com" });
 console.log(user);
+user.sendMessage();
+
 // executar o comando "./node_modules/.bin/babel main.js --out-dir dist" no terminal para que o babel gere o código a ser lido por navegadores incompatíveis com essa versão do ECMAScript
