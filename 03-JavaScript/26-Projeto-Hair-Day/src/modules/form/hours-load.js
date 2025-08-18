a457e6113cd68374085a9ff7b368b3a8a6a2b103
Davi <davi.rferreira@gmail.com>
2025-08-18 12:00:00 -0300

    Projeto-Hair-Day - Renderizando os horários

--- 03-JavaScript/26-Projeto-Hair-Day/src/modules/form/hours-load.js
+++ 03-JavaScript/26-Projeto-Hair-Day/src/modules/form/hours-load.js
@@ -1,6 +1,9 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+// Captura a lista do formulário
+const hours = document.querySelector("ul#hours")
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
@@ -15,5 +18,15 @@ export function hoursLoad({ date }) {
       avaiable: isHourPast
     }
   })
-  console.log(opening);
+
+  // Renderizar os horários
+  opening.forEach(({ hour, avaiable }) => {
+    const li = document.createElement("li")
+    li.classList.add(
+      "hour",
+      (avaiable ? "hour-available" : "hour-unavailable")
+    )
+    li.textContent = `${hour}`
+    hours.append(li);
+  })
 }
--- 03-JavaScript/26-Projeto-Hair-Day/src/modules/schedules/load.js
+++ 03-JavaScript/26-Projeto-Hair-Day/src/modules/schedules/load.js
@@ -1,7 +1,15 @@
+import dayjs from "dayjs";
 import { hoursLoad } from "../form/hours-load"
 
 // Seleciona o input de data
 const selectedDate = document.querySelector("input[type=date]");
+
+// Data atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// Carrega a data atual e define a data mínima como sendo a data atual
+selectedDate.value = inputToday
+selectedDate.min = inputToday
 
 export function schedulesDay() {
   // Obtem a data do input
