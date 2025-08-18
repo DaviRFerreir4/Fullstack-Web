50033063e39bb3873f6815052de580ddaa36bd87
Davi <davi.rferreira@gmail.com>
2025-08-18 13:58:21 -0300

    Projeto-Hair-Day - Criando o serviço de agendamento

---
+++ 03-JavaScript/26-Projeto-Hair-Day/src/services/schedule-new.js
@@ -0,0 +1,18 @@
+import { apiConfig } from "./api-config"
+
+export async function scheduleNew({ id, name, when }){
+  try {
+    await fetch(`${apiConfig.baseURL}/schedules`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({id, name, when}),
+    })
+
+    alert("Agendamento realizado com sucesso!")
+  } catch(error) {
+    alert("Não foi possível registar o agendamento. Tente novamente mais tarde.")
+    console.log(error);
+  }
+}
