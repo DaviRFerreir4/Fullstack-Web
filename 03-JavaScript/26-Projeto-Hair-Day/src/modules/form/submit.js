285c08c1082dd218258767abde2dd890d92f9f5e
Davi <davi.rferreira@gmail.com>
2025-08-18 13:44:06 -0300

    Projeto-Hair-Day - Recuperando os dados para envio para a API

--- 03-JavaScript/26-Projeto-Hair-Day/src/modules/form/submit.js
+++ 03-JavaScript/26-Projeto-Hair-Day/src/modules/form/submit.js
@@ -1,7 +1,44 @@
-const form = document.querySelector("form");
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const clientName = document.querySelector("input#client")
 const selectedDate = document.querySelector("input[type=date]")
 
 form.onsubmit = (event) => {
   // Previne o comportamento padrão do formulário
   event.preventDefault()
+
+  try {
+    // Recupera o valor e testa se ele é vazio
+    const name = clientName.value.trim()
+    if(!name) {
+      return alert("Informe o nome do cliente!")
+    }
+
+    // Recupera o horário selecionado e testa se ele é vazio
+    const hourSelected = document.querySelector(".hour-selected")
+    if(!hourSelected) {
+      return alert("Selecione a hora!")
+    }
+
+    // Recupera somente a hora
+    const [ hour ] = hourSelected.innerText.split(":")
+
+    // Insere a hora na data
+    const date = selectedDate.value
+    const when = dayjs(date).add(hour, "hour");
+
+    // Gera um ID
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name,
+      when
+    });
+    
+  } catch(error) {
+    alert("Não foi possível realizar o agendamento.")
+    console.log(error)
+  }
 }
