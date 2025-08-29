8da0bb8be28ea64a9f3c21a8741baabffa58340f
Davi <davi.rferreira@gmail.com>
2025-08-29 10:42:28 -0300

    05-Node | 02-Fundamentos-API - Separando rotas

---
+++ 05-Iniciando-no-NodeJs/Primeira-API/src/routes.js
@@ -0,0 +1,17 @@
+// Separando rotas
+export const routes = [
+  {
+    method: "GET",
+    path: "/products",
+    controller: (request, response) => {
+      return response.end("Lista de produtos: ...")
+    },
+  },
+  {
+    method: "POST",
+    path: "/products",
+    controller: (request, response) => {
+      return response.writeHead(201).end(JSON.stringify(request.body))
+    },
+  },
+]
