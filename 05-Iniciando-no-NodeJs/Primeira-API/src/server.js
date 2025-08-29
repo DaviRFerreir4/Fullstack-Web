e208f81d60d22e9bdbba331ff9cfcf82dc0f51a6
Davi <davi.rferreira@gmail.com>
2025-08-29 10:24:51 -0300

    05-Node | 02-Fundamentos-API - URL da requisição

--- 05-Iniciando-no-NodeJs/Primeira-API/src/server.js
+++ 05-Iniciando-no-NodeJs/Primeira-API/src/server.js
@@ -2,9 +2,16 @@ import http from "node:http"
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
 
 const server = http.createServer(async (request, response) => {
-  // Utilizando Status Code
-  const { method } = request
-  return response.writeHead(200).end("Método: " + method)
+  // URL da requisição
+  const { method, url } = request
+  // return response.writeHead(200).end("URL: " + url)
+  if (method === "GET" && url === "/products") {
+    return response.end("Lista de produtos: ...")
+  }
+  if (method === "POST" && url === "/products") {
+    return response.writeHead(201).end("Produto cadastrado com sucesso!")
+  }
+  return response.writeHead(404).end("Rota não encontrada!")
 })
 
 server.listen(3333)
