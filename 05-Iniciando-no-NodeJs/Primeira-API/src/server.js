4c0d35b2d86bd8363446b164238f6fd51be33157
Davi <davi.rferreira@gmail.com>
2025-08-29 10:25:39 -0300

    05-Node | 02-Fundamentos-API - Recuperando dados no body

--- 05-Iniciando-no-NodeJs/Primeira-API/src/server.js
+++ 05-Iniciando-no-NodeJs/Primeira-API/src/server.js
@@ -2,13 +2,17 @@ import http from "node:http"
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
 
 const server = http.createServer(async (request, response) => {
-  // URL da requisição
+  // Recuperando dados no body
   const { method, url } = request
-  // return response.writeHead(200).end("URL: " + url)
   if (method === "GET" && url === "/products") {
     return response.end("Lista de produtos: ...")
   }
   if (method === "POST" && url === "/products") {
+    const buffers = []
+    for await (const chunck of request) {
+      buffers.push(chunck)
+    }
+    console.log(Buffer.concat(buffers).toString())
     return response.writeHead(201).end("Produto cadastrado com sucesso!")
   }
   return response.writeHead(404).end("Rota não encontrada!")
