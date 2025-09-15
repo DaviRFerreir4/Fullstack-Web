4280d15ebeb98fd1c85165a5c247938c5ce8bcd2
Davi <davi.rferreira@gmail.com>
2025-09-15 15:38:02 -0300

    05-Node | 04 - Projeto-Tickets-de-Suporte - Atualizando dados do ticket

--- 05-Iniciando-no-NodeJs/03-Projeto-Tickets-de-Suporte/src/controllers/tickets/update.js
+++ 05-Iniciando-no-NodeJs/03-Projeto-Tickets-de-Suporte/src/controllers/tickets/update.js
@@ -2,5 +2,10 @@ export function update({ request, response, database }) {
   const { id } = request.params
   const { equipment } = request.body
 
+  database.update('tickets', id, {
+    equipment,
+    updatedAt: new Date(),
+  })
+
   return response.end()
 }
