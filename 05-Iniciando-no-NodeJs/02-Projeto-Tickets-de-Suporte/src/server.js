06ada0a0db5e5445c16f49c034ecebb5a48b6fe2
Davi <davi.rferreira@gmail.com>
2025-09-15 09:39:09 -0300

    05-Node | 04 - Projeto-Tickets-de-Suporte - Separando os controllers das rotas

--- 05-Iniciando-no-NodeJs/02-Projeto-Tickets-de-Suporte/src/server.js
+++ 05-Iniciando-no-NodeJs/02-Projeto-Tickets-de-Suporte/src/server.js
@@ -4,8 +4,8 @@ import { jsonHandler } from './middlewares/jsonHandler.js'
 import { routeHandler } from './middlewares/routeHandler.js'
 
 async function listener(request, response) {
-  await jsonHandler(request, response)
-  routeHandler(request, response)
+  await jsonHandler({ request, response })
+  routeHandler({ request, response })
 }
 
 http.createServer(listener).listen(3333)
