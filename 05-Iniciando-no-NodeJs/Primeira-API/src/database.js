1016bdaabc724463f671d7fc46b1c8b1edddc9f4
Davi <davi.rferreira@gmail.com>
2025-09-15 08:55:51 -0300

    05-Node | 03-Manipulando Dados - Propriedade e método privados

--- 05-Iniciando-no-NodeJs/Primeira-API/src/database.js
+++ 05-Iniciando-no-NodeJs/Primeira-API/src/database.js
@@ -1,10 +1,10 @@
-// Lendo o conteúdo do arquivo
+// Propriedade e método privados
 import fs from 'node:fs/promises'
 
 const DATABASE_PATH = new URL('db.json', import.meta.url)
 
 export class Database {
-  database = {}
+  #database = {}
 
   constructor() {
     fs.readFile(DATABASE_PATH, 'utf-8')
@@ -12,24 +12,24 @@ export class Database {
         console.log(JSON.parse(data))
       })
       .catch(() => {
-        this.persist()
+        this.#persist()
       })
   }
 
-  persist() {
-    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))
   }
 
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
-      this.database[table].push(data)
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
     } else {
-      this.database[table] = [data]
+      this.#database[table] = [data]
     }
-    this.persist()
+    this.#persist()
   }
 
   select(table) {
-    return this.database[table]
+    return this.#database[table] ?? []
   }
 }
