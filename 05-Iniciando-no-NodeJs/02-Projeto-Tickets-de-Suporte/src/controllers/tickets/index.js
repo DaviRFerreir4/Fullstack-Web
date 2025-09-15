677467f071b80399b7df37655fbd8a47a9ab010d
Davi <davi.rferreira@gmail.com>
2025-09-15 10:59:06 -0300

    05-Node | 04 - Projeto-Tickets-de-Suporte - Obtendo parâmetros da rota

--- 05-Iniciando-no-NodeJs/02-Projeto-Tickets-de-Suporte/src/controllers/tickets/index.js
+++ 05-Iniciando-no-NodeJs/02-Projeto-Tickets-de-Suporte/src/controllers/tickets/index.js
@@ -1,4 +1,6 @@
 export function index({ request, response, database }) {
+  const { status } = request.query
+  console.log(status)
   const tickets = database.select()
   return response.end(JSON.stringify(tickets))
 }
