6bde7a50b27b632f5f1d01715e0f5ad744f2ce36
Davi <davi.rferreira@gmail.com>
2025-09-15 08:23:58 -0300

    05-Node | 03-Manipulando Dados - Criando o arquivo de dados

--- 05-Iniciando-no-NodeJs/Primeira-API/src/database.js
+++ 05-Iniciando-no-NodeJs/Primeira-API/src/database.js
@@ -1,6 +1,18 @@
-// Salvando dados em memória
+// Criando o arquivo de dados
+import fs from 'node:fs/promises'
+
+const DATABASE_PATH = new URL('db.json', import.meta.url)
+
 export class Database {
   database = {}
+
+  constructor() {
+    this.persist()
+  }
+
+  persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+  }
 
   insert(table, data) {
     if (Array.isArray(this.database[table])) {
