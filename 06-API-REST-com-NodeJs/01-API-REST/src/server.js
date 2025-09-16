12659164d17c0a4ea35c44215b8593bcbe87672d
Davi <davi.rferreira@gmail.com>
2025-09-16 10:11:10 -0300

    06-API-REST | 02-TypeScript - Executando TypeScript com tsx

--- 06-API-REST-com-NodeJs/01-API-REST/src/server.js
+++
@@ -1,5 +0,0 @@
-function sum(a, b) {
-    return a + b;
-}
-var result = sum(4, 5);
-console.log('Resultado da soma:', result);
--- 06-API-REST-com-NodeJs/01-API-REST/src/server.ts
+++ 06-API-REST-com-NodeJs/01-API-REST/src/server.ts
@@ -2,5 +2,5 @@ function sum(a: number, b: number): number {
   return a + b
 }
 
-const result: number = sum(4, 5)
+const result: number = sum(2, 5)
 console.log('Resultado da soma:', result)
