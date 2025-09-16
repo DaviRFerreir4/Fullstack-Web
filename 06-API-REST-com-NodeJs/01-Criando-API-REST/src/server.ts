7a8646153d972c4e2127c7364e50d0e04d92c679
Davi <davi.rferreira@gmail.com>
2025-09-16 13:19:20 -0300

    06-API-REST | 03-Express - Recuperando parâmetros da requisição

--- 06-API-REST-com-NodeJs/01-Criando-API-REST/src/server.ts
+++ 06-API-REST-com-NodeJs/01-Criando-API-REST/src/server.ts
@@ -9,19 +9,28 @@ app.get('/', (request, response) => {
   response.send('Hello World Express')
 })
 
-// GET com parâmetros nomeados
+// GET com parâmetros nomeados (route params)
 app.get('/products/:id', (request, response) => {
   const { id } = request.params
 
-  response.send(`Hello World Express. Your ID is ${id}`)
+  response.send(`Hello World Express. Seu ID é ${id}`)
 })
 
 app.get('/products/:id/:user', (request, response) => {
   const { id, user } = request.params
 
-  response.send(
-    `Hello World Express. Your ID is ${id} and your user is ${user}`
-  )
+  response.send(`Hello World Express. Seu ID é ${id} e seu usuário é ${user}`)
+})
+
+// GET com parâmetros não nomeados (query params)
+app.get('/products', (request, response) => {
+  const { page, limit } = request.query
+
+  if (page && limit) {
+    response.send(`Página ${page} de ${limit}`)
+  }
+
+  response.send('Listando produtos...')
 })
 
 app.listen(PORT, () => {
