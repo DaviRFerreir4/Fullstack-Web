575117ff1d21d8f4aac34a590f57ea8481138adc
Davi <davi.rferreira@gmail.com>
2025-09-16 14:15:12 -0300

    06-API-REST | 03-Express - Utilizando controllers separados em arquivos

---
+++ 06-API-REST-com-NodeJs/01-Criando-API-REST/src/controlers/products-controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+
+class ProductController {
+  /**
+   * index - GET para listar vários registros
+   * show - GET para exibir um registro específico
+   * create - POST para criar um registro
+   * update - PUT para atualizar um registro
+   * remove - DELETE para deletar um registro
+   */
+
+  index(request: Request, response: Response) {
+    const { page, limit } = request.query
+
+    if (page && limit) {
+      response.send(`Página ${page} de ${limit}`)
+    } else {
+      response.send(`Listando produtos...`)
+    }
+  }
+
+  create(request: Request, response: Response) {
+    const { name, price } = request.body
+
+    // response.send(`Produto ${name} custa $${price}`)
+    response.status(201).json({
+      name,
+      price,
+      user_id: request.user_id,
+    })
+  }
+}
+
+export { ProductController }
--- 06-API-REST-com-NodeJs/01-Criando-API-REST/src/routes/products-routes.ts
+++ 06-API-REST-com-NodeJs/01-Criando-API-REST/src/routes/products-routes.ts
@@ -1,29 +1,13 @@
 import { Router } from 'express'
 import myMiddleware from '../middlewares/my-middleware.js'
+import { ProductController } from '../controlers/products-controller.js'
 
 const productRoutes = Router()
+const productController = new ProductController()
 
-productRoutes.get('/:id', (request, response) => {
-  const { id } = request.params
-  const { page, limit } = request.query
+productRoutes.get('/', productController.index)
 
-  if (page && limit) {
-    response.send(`Página ${page} de ${limit} | ID = ${id}`)
-  } else {
-    response.send(`Listando produtos... | ID = ${id}`)
-  }
-})
-
-// Recuperando dados do corpo da requisição POST e utilizando um middleware local em uma rota específica
-productRoutes.post('', myMiddleware, (request, response) => {
-  const { name, price } = request.body
-
-  // response.send(`Produto ${name} custa $${price}`)
-  response.status(201).json({
-    name,
-    price,
-    user_id: request.user_id,
-  })
-})
+// Middleware local em uma rota específica
+productRoutes.post('/', myMiddleware, productController.create)
 
 export { productRoutes }
