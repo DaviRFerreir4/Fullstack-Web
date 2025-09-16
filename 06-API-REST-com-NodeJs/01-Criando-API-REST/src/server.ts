816465ccf211b0c3dd9f05046605eb20bc554b2b
Davi <davi.rferreira@gmail.com>
2025-09-16 13:27:20 -0300

    06-API-REST | 03-Express - Resposta com JSON e Status Code específico

--- 06-API-REST-com-NodeJs/01-Criando-API-REST/src/server.ts
+++ 06-API-REST-com-NodeJs/01-Criando-API-REST/src/server.ts
@@ -39,7 +39,11 @@ app.get('/products', (request, response) => {
 app.post('/products', (request, response) => {
   const { name, price } = request.body
 
-  response.send(`Produto ${name} custa $${price}`)
+  // response.send(`Produto ${name} custa $${price}`)
+  response.status(201).json({
+    name,
+    price,
+  })
 })
 
 app.listen(PORT, () => {
