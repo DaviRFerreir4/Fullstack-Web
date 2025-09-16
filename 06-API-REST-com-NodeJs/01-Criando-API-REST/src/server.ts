d61eb18c2a83356b7e983e3b292be35fc50c525e
Davi <davi.rferreira@gmail.com>
2025-09-16 13:14:57 -0300

    06-API-REST | 03-Express - Recuperando parâmetros da rota

--- 06-API-REST-com-NodeJs/01-Criando-API-REST/src/server.ts
+++ 06-API-REST-com-NodeJs/01-Criando-API-REST/src/server.ts
@@ -4,8 +4,24 @@ const PORT = 3333
 
 const app = express()
 
+// GET simples
 app.get('/', (request, response) => {
   response.send('Hello World Express')
+})
+
+// GET com parâmetros nomeados
+app.get('/products/:id', (request, response) => {
+  const { id } = request.params
+
+  response.send(`Hello World Express. Your ID is ${id}`)
+})
+
+app.get('/products/:id/:user', (request, response) => {
+  const { id, user } = request.params
+
+  response.send(
+    `Hello World Express. Your ID is ${id} and your user is ${user}`
+  )
 })
 
 app.listen(PORT, () => {
