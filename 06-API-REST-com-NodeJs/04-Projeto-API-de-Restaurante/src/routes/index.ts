d938a38f2b3059eb67011e51427f77ffc056e61d
Davi <davi.rferreira@gmail.com>
2025-10-06 15:10:41 -0300

    Projeto-API-de-Restaurante - Rota para listagem de mesas

--- 06-API-REST-com-NodeJs/04-Projeto-API-de-Restaurante/src/routes/index.ts
+++ 06-API-REST-com-NodeJs/04-Projeto-API-de-Restaurante/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { productRoutes } from './products-routes'
+import { tablesRoutes } from './table-routes'
 
 const routes = Router()
 routes.use('/products', productRoutes)
+routes.use('/tables', tablesRoutes)
 
 export { routes }
