e13059e63c9af603d639a1bb659d2fdf177d2843
Davi <davi.rferreira@gmail.com>
2025-10-13 10:55:20 -0300

    07-NodeJs-com-Containers | 04-Testes - Agrupando testes e alternativa a função test

--- 07-NodeJs-com-Containers/04-Testes-Automatizados/src/sum.test.ts
+++ 07-NodeJs-com-Containers/04-Testes-Automatizados/src/sum.test.ts
@@ -1,8 +1,17 @@
 import { sum } from './server'
 
-test('sum of 3 + 7 must be 10', () => {
-  const result = sum(3, 7)
+describe('sum', () => {
+  // it = test. É mais utilizado por ser mais semântico
+  it('should do sum of 3 + 7 must be 10', () => {
+    const result = sum(3, 7)
 
-  expect(result).toBe(10)
-  // expect(result).toBe(12) // retorna falha. Sempre testar o verdadeiro e o falso dos testes para evitar falsos verdadeiros
+    expect(result).toBe(10)
+    // expect(result).toBe(12) // retorna falha. Sempre testar o verdadeiro e o falso dos testes para evitar falsos verdadeiros
+  })
+
+  test('sum of 2 + 2 must be 4', () => {
+    const result = sum(2, 2)
+
+    expect(result).toBe(4)
+  })
 })
