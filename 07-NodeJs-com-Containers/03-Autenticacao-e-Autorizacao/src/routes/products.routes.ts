1fbb953af45c64174861277b718fd045b2993486
Davi <davi.rferreira@gmail.com>
2025-10-13 10:16:44 -0300

    07-NodeJs-com-Containers | 03-Auth - Middleware de autorização

--- 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/routes/products.routes.ts
+++ 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/routes/products.routes.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express'
 import { ProductsControllers } from '@/controllers/products.controller.js'
-
 import { ensureAuthenticated } from '@/middlewares/ensureAuthenticated.js'
+import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization.js'
 
 const productsRoutes = Router()
 const productsControllers = new ProductsControllers()
 
+// Para aplicar autorização em todas as rotas seguintes
+// productsRoutes.use(verifyUserAuthorization(['sales', 'admin']))
+
 productsRoutes.get('/', productsControllers.index)
-productsRoutes.post('/', ensureAuthenticated, productsControllers.create)
+
+// Autenticação e autorização em uma rota específica
+productsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  verifyUserAuthorization(['sale', 'admin']),
+  productsControllers.create
+)
 
 export { productsRoutes }
