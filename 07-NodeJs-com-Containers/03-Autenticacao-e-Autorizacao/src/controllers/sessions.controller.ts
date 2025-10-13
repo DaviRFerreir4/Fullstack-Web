28ae4aa73d2a35a3f1da93495c7680aa873c85c2
Davi <davi.rferreira@gmail.com>
2025-10-13 09:50:53 -0300

    07-NodeJs-com-Containers | 03-Auth - Configuração do jwt e criação de um token utilizando as configurações

--- 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/controllers/sessions.controller.ts
+++ 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/controllers/sessions.controller.ts
@@ -1,7 +1,31 @@
 import type { Request, Response } from 'express'
+import { AppError } from '@/utils/AppError.js'
+import { authConfig } from '@/configs/auth.js'
+import jwt from 'jsonwebtoken'
 
 class SessionsControllers {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const fakeUser = {
+      id: 1,
+      username: 'Davi',
+      password: '123456',
+    }
+
+    const { username, password } = request.body
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError('Usuário e/ou senha estão incorretos', 401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = jwt.sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    })
+
+    return response.status(201).json({ token: token })
+  }
 }
 
 export { SessionsControllers }
