44de71f9b4502162bc2ec9cd6a774f8b610dc889
Davi <davi.rferreira@gmail.com>
2025-10-13 09:50:16 -0300

    07-NodeJs-com-Containers | 03-Auth - Arrumando problemas relacionados ao express-async-errors

--- 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/controllers/products.controller.ts
+++ 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/controllers/products.controller.ts
@@ -1,9 +1,13 @@
 import type { Request, Response } from 'express'
 
 class ProductsControllers {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    return response.json()
+  }
 
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    return response.status(201).json()
+  }
 }
 
 export { ProductsControllers }
--- 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/server.ts
+++ 07-NodeJs-com-Containers/03-Autenticacao-e-Autorizacao/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import type { Request, Response } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
+import { AppError } from './utils/AppError.js'
 import { routes } from './routes/index.js'
 
 const PORT = 3333
@@ -11,7 +12,11 @@ app.use(express.json())
 
 app.use(routes)
 
-app.use((error: any, request: Request, response: Response) => {
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message })
+  }
+
   return response.status(500).json({ message: error.message })
 })
 
