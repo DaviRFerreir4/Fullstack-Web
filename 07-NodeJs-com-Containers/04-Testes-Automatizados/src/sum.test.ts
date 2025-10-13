09006b98deaf26e26b0c7dd331606f868911b2bd
Davi <davi.rferreira@gmail.com>
2025-10-13 11:02:18 -0300

    07-NodeJs-com-Containers | 04-Testes - Utilizando beforeAll e afterAll para manipular uma variável utilizada num teste

--- 07-NodeJs-com-Containers/04-Testes-Automatizados/src/sum.test.ts
+++ 07-NodeJs-com-Containers/04-Testes-Automatizados/src/sum.test.ts
@@ -1,11 +1,23 @@
 import { sum } from './server'
 
 describe('sum', () => {
+  let sumResult: number
+
+  beforeAll(() => {
+    sumResult = 10
+    console.log('Executado antes dos testes', sumResult)
+  })
+
+  afterAll(() => {
+    sumResult = 0
+    console.log('Executado depois dos testes', sumResult)
+  })
+
   // it = test. É mais utilizado por ser mais semântico
   it('should do sum of 3 + 7 must be 10', () => {
     const result = sum(3, 7)
 
-    expect(result).toBe(10)
+    expect(result).toBe(sumResult)
     // expect(result).toBe(12) // retorna falha. Sempre testar o verdadeiro e o falso dos testes para evitar falsos verdadeiros
   })
 
