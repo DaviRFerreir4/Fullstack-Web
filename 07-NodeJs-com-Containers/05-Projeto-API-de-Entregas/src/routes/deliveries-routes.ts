65fd478cd8263d90c4596ed010cc496edc0502f5
Davi <davi.rferreira@gmail.com>
2025-10-15 10:51:27 -0300

    Projeto-API-de-Entregas - Adicionando status as entregas (tinha esquecido) e rota para atualizar status de entregas

--- 07-NodeJs-com-Containers/05-Projeto-API-de-Entregas/src/routes/deliveries-routes.ts
+++ 07-NodeJs-com-Containers/05-Projeto-API-de-Entregas/src/routes/deliveries-routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { DeliveriesController } from '@/controllers/deliveries-controller'
+import { DeliveriesStatusController } from '@/controllers/deliveries-status-controller'
 import { ensureAuthenticated } from '@/middlewares/ensure-authenticated'
 import { verifyUserAuthorization } from '@/middlewares/verify-user-authorization'
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController()
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(['sale']))
 deliveriesRoutes.get('/', deliveriesController.index)
 deliveriesRoutes.post('/', deliveriesController.create)
+deliveriesRoutes.patch('/:id/status', deliveriesStatusController.update)
 
 export { deliveriesRoutes }
