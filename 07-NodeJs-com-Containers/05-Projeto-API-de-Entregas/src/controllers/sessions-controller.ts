f92b8214e179a89c0062c8039e0f493c9444f4f5
Davi <davi.rferreira@gmail.com>
2025-10-15 09:22:07 -0300

    Projeto-API-de-Entregas - Rota para criação de sessão de usuário

--- 07-NodeJs-com-Containers/05-Projeto-API-de-Entregas/src/controllers/sessions-controller.ts
+++ 07-NodeJs-com-Containers/05-Projeto-API-de-Entregas/src/controllers/sessions-controller.ts
@@ -1,8 +1,33 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+import { AppError } from '@/utils/app-error'
+import { prisma } from '@/database/prisma'
+import { compare } from 'bcrypt'
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.status(201).json()
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().trim().email(),
+      password: z.string().trim().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    })
+
+    if (!user) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    return response.status(201).json({ message: 'ok' })
   }
 }
 
