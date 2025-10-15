1b48b33a5dc6b6f3626be9c9a1a2d348b22a668c
Davi <davi.rferreira@gmail.com>
2025-10-15 08:12:06 -0300

    Projeto-API-de-Entregas - Validando dados do corpo da requisição da rota de criação de usuários

--- 07-NodeJs-com-Containers/05-Projeto-API-de-Entregas/src/controllers/users-controllers.ts
+++ 07-NodeJs-com-Containers/05-Projeto-API-de-Entregas/src/controllers/users-controllers.ts
@@ -1,7 +1,16 @@
 import type { Request, Response } from 'express'
+import { z } from 'zod'
 
 class UserControllers {
   create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim(),
+      email: z.string().trim().email(),
+      password: z.string().trim().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
     return response.status(201).json({ message: 'created' })
   }
 }
