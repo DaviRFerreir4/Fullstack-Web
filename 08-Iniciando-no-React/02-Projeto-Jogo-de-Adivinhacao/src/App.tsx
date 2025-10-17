072fa95a5a480fe300945db9b7befabeba7e77b1
Davi <davi.rferreira@gmail.com>
2025-10-17 10:54:38 -0300

    Jogo-de-Adivinhacao - Header dinâmico utilizando props

--- 08-Iniciando-no-React/02-Projeto-Jogo-de-Adivinhacao/src/App.tsx
+++ 08-Iniciando-no-React/02-Projeto-Jogo-de-Adivinhacao/src/App.tsx
@@ -2,10 +2,14 @@ import styles from './app.module.css'
 import { Header } from './components/Header'
 
 export function App() {
+  function handleGameRestart() {
+    alert('jogo reiniciado')
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={handleGameRestart} />
       </main>
     </div>
   )
--- 08-Iniciando-no-React/02-Projeto-Jogo-de-Adivinhacao/src/components/Header/index.tsx
+++ 08-Iniciando-no-React/02-Projeto-Jogo-de-Adivinhacao/src/components/Header/index.tsx
@@ -2,17 +2,21 @@ import styles from './style.module.css'
 import logo from '../../assets/logo.png'
 import resetIcon from '../../assets/restart.svg'
 
-export function Header() {
+type Props = {
+  current: number
+  max: number
+  onRestart: () => void
+}
+
+export function Header({ current = 0, max = 10, onRestart }: Props) {
   return (
     <header className={styles.container}>
       <img src={logo} alt="Logo da aplicação" />
-
       <div>
         <span>
-          <strong>5</strong> de 10 tentativas
+          <strong>{current}</strong> de {max} tentativas
         </span>
-
-        <button type="button">
+        <button type="button" onClick={onRestart}>
           <img src={resetIcon} alt="Icone de reiniciar" />
         </button>
       </div>
