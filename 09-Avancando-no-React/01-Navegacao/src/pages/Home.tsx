9f92470a017016211282f77116f23f07190313c8
Davi <davi.rferreira@gmail.com>
2025-10-20 13:06:49 -0300

    09-Avancando-no-React | 01-Navegacao - Navegando com useNavigate

--- 09-Avancando-no-React/01-Navegacao/src/pages/Home.tsx
+++ 09-Avancando-no-React/01-Navegacao/src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router'
+
 export function Home() {
+  const navigate = useNavigate()
+
+  function productOpen() {
+    navigate('/products')
+  }
+
   return (
     <div>
       <nav>
         <a href="/products">Produtos</a>
         <a href="/products?category=tvs&price=2000">Categoria</a>
+        <button type="button" onClick={productOpen}>
+          Ver produtos
+        </button>
       </nav>
       <h1>Página Home</h1>
     </div>
