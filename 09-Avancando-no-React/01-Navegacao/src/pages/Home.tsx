8ecc58180a6f2b7e8b0c3b201fca1a1e2c728705
Davi <davi.rferreira@gmail.com>
2025-10-20 10:36:33 -0300

    09-Avancando-no-React | 01-Navegacao - Recuperando parâmetros opcionais (query params)

--- 09-Avancando-no-React/01-Navegacao/src/pages/Home.tsx
+++ 09-Avancando-no-React/01-Navegacao/src/pages/Home.tsx
@@ -3,6 +3,7 @@ export function Home() {
     <div>
       <nav>
         <a href="/products">Produtos</a>
+        <a href="/products?category=tvs&price=2000">Categoria</a>
       </nav>
       <h1>Página Home</h1>
     </div>
--- 09-Avancando-no-React/01-Navegacao/src/pages/Product.tsx
+++ 09-Avancando-no-React/01-Navegacao/src/pages/Product.tsx
@@ -1,10 +1,27 @@
+import { useSearchParams } from 'react-router'
+
 export function Product() {
+  const [searcParams] = useSearchParams()
+
+  const category = searcParams.get('category')
+  const price = searcParams.get('price')
+
   return (
     <div>
       <nav>
         <a href="/">Voltar</a>
       </nav>
       <h1>Produtos</h1>
+      {category && (
+        <p>
+          Categoria: <strong>{category}</strong>
+        </p>
+      )}
+      {price && (
+        <p>
+          Preço: <strong>{price}</strong>
+        </p>
+      )}
     </div>
   )
 }
