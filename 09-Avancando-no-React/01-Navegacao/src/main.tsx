3af92cc00ab99299d4f88c05277527a95a6a4669
Davi <davi.rferreira@gmail.com>
2025-10-20 10:18:47 -0300

    09-Avancando-no-React | 01-Navegacao - Configurando rotas e criando a rota Home

--- 09-Avancando-no-React/01-Navegacao/src/main.tsx
+++ 09-Avancando-no-React/01-Navegacao/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 
-import { App } from './App.tsx'
+import { AppRoutes } from './routes/appRoutes'
 import './global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>
 )
