b24f0d1e572109bb6321c9140d11f7f4147733ce
Davi <davi.rferreira@gmail.com>
2025-10-21 10:41:20 -0300

    Refund-2.0 - Link para tela de sign up e alterações no layout

--- 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/SignIn.tsx
+++ 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/SignIn.tsx
@@ -21,26 +21,37 @@ export function SignIn() {
   }
 
   return (
-    <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
-      <Input
-        required
-        legend="E-mail"
-        type="email"
-        placeholder="seu@email.com"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        required
-        legend="Senha"
-        type="password"
-        placeholder="******"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button type="submit" isLoading={isLoading}>
-        Entrar
-      </Button>
-    </form>
+    <>
+      <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
+        <Input
+          required
+          legend="E-mail"
+          type="email"
+          placeholder="seu@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          required
+          legend="Senha"
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button type="submit" isLoading={isLoading}>
+          Entrar
+        </Button>
+      </form>
+
+      <a
+        href="/signup"
+        className="text-sm text-semibold text-gray-100 mt-8 hover:text-green-800 transition ease-linear w-fit"
+      >
+        Criar conta
+      </a>
+    </>
   )
 }
