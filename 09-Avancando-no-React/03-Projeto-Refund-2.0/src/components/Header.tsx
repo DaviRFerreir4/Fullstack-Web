7cc3bbcdf887b90ca3fe628b6712123bf46ad062
Davi <davi.rferreira@gmail.com>
2025-10-21 15:07:10 -0300

    Refund-2.0 - Tela de confirmação da solicitação finalizada

--- 09-Avancando-no-React/03-Projeto-Refund-2.0/src/components/Header.tsx
+++ 09-Avancando-no-React/03-Projeto-Refund-2.0/src/components/Header.tsx
@@ -1,11 +1,11 @@
 import logoSvg from '../assets/logo.svg'
 import logoutIconSvg from '../assets/logout.svg'
 
-type Props = {}
+// type Props = {}
 
 export function Header() {
   return (
-    <header className="w-full flex justify-between">
+    <header className="w-full flex justify-between md:px-22.5 px-3">
       <img src={logoSvg} alt="Logo" className="my-8" />
       <div className="flex items-center gap-3">
         <span className="text-sm font-semibold text-gray-200">Olá, Davi</span>
--- 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/Confirm.tsx
+++ 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/Confirm.tsx
@@ -1,11 +1,39 @@
-import { Navigate, useLocation } from 'react-router'
+import { Navigate, useLocation, useNavigate } from 'react-router'
+
+import okIconSvg from '../assets/ok.svg'
+import { Button } from '../components/Button'
 
 export function Confirm() {
   const location = useLocation()
+  const navigate = useNavigate()
 
   if (!location.state?.fromSubmit) {
     return <Navigate to="/" />
   }
 
-  return <h1>Confirm</h1>
+  return (
+    <div className="bg-gray-500 lg:w-[512px] roundex-xl flex flex-col items-center p-10 gap-6">
+      <h1 className="text-2xl font-bold text-center text-green-100">
+        Solicitação enviada
+      </h1>
+
+      <img src={okIconSvg} alt="Icone de OK" className="w-28" />
+
+      <p className="text-sm text-gray-200 text-center">
+        Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o
+        setor financeiro irá entrar em contato com você.
+      </p>
+
+      <div className="w-full flex flex-col">
+        <Button
+          type="button"
+          onClick={() => {
+            navigate('/')
+          }}
+        >
+          Nova Solicitação
+        </Button>
+      </div>
+    </div>
+  )
 }
