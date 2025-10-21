322bd4cb4f11b8e7dc7fee9415dfc4ddcef973f1
Davi <davi.rferreira@gmail.com>
2025-10-21 09:09:10 -0300

    Refund-2.0 - Componente de button recuperando informações para submit do formulário

--- 09-Avancando-no-React/03-Projeto-Refund-2.0/src/components/Input.tsx
+++ 09-Avancando-no-React/03-Projeto-Refund-2.0/src/components/Input.tsx
@@ -2,14 +2,14 @@ type Props = React.ComponentProps<'input'> & {
   legend?: string
 }
 
-export function Input({ legend, ...rest }: Props) {
+export function Input({ legend, type = 'text', ...rest }: Props) {
   return (
     <fieldset className="flex flex-1 max-h-20 text-gray-200 focus-within:text-green-100">
       {legend && (
         <legend className="uppercase text-xxs mb-2 font-bold">{legend}</legend>
       )}
       <input
-        type="text"
+        type={type}
         className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent placeholder-gray-300 outline-1 outline-transparent focus:outline-green-100 focus:border-green-100"
         {...rest}
       />
--- 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/SignIn.tsx
+++ 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/SignIn.tsx
@@ -1,15 +1,46 @@
 import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+import { useState } from 'react'
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+
+    setIsLoading(true)
+
+    console.log({
+      email,
+      password,
+    })
+
+    setTimeout(() => setIsLoading(false), 2000)
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
       <Input
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <Input required legend="Senha" type="password" placeholder="******" />
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="******"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
     </form>
   )
 }
