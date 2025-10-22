93a627b8a4c9c5cbff7315d92086cc4d0c0657ce
Davi <davi.rferreira@gmail.com>
2025-10-22 09:04:53 -0300

    Refund-2.0 - Input para pesqusa de solicitação

--- 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/Dashboard.tsx
+++ 09-Avancando-no-React/03-Projeto-Refund-2.0/src/pages/Dashboard.tsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+import { Input } from '../components/Input'
+
 export function Dashboard() {
+  const [name, setName] = useState('')
+
+  function fetchRefunds(e: React.FormEvent) {
+    e.preventDefault()
+
+    console.log(name)
+  }
+
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
+
+      <form
+        className="flex items-center justify-between pb-6 border-b border-b-gray-400 md:flex-row gap-2  mt-6"
+        onSubmit={fetchRefunds}
+      >
+        <Input
+          placeholder="Pesquisar pelo nome"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+        />
+      </form>
     </div>
   )
 }
