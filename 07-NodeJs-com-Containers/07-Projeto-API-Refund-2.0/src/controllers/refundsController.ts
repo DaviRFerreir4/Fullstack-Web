380b426d67176cc5314c03fdc782bfd15f0ae12b
Davi <davi.rferreira@gmail.com>
2025-10-23 09:24:19 -0300

    API-Refund-2.0 - Rota para criação de refunds com validação de dados da requisição

--- 07-NodeJs-com-Containers/07-Projeto-API-Refund-2.0/src/controllers/refundsController.ts
+++ 07-NodeJs-com-Containers/07-Projeto-API-Refund-2.0/src/controllers/refundsController.ts
@@ -1,8 +1,52 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+import { Category } from '@prisma/client'
+
+import { prisma } from '@/database/prisma.js'
+import { AppError } from '@/utils/AppError.js'
 
 class RefundsController {
   async create(request: Request, response: Response) {
-    return response.status(201).json()
+    const bodySchema = z.object({
+      name: z.string({ message: 'Nome é obrigatório' }).trim(),
+      category: z.enum(
+        [
+          Category.accomodation,
+          Category.food,
+          Category.others,
+          Category.services,
+          Category.transport,
+        ],
+        {
+          message:
+            "Categoria deve ser 'accomodation', 'food', 'services', 'transport' ou 'others'",
+        }
+      ),
+      amount: z
+        .number({ message: 'Valor é obrigatório e deve ser um número' })
+        .gt(0, { message: 'Valor deve ser maior que 0' }),
+      filename: z.string({ message: 'Nome do arquivo é obrigatório' }).min(20, {
+        message: 'O nome do arquivo deve ser maior que 20 digitos',
+      }),
+    })
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body)
+
+    if (!request.user?.id) {
+      throw new AppError('Não autorizado', 401)
+    }
+
+    const refund = await prisma.refund.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: request.user.id,
+      },
+    })
+
+    return response.status(201).json(refund)
   }
 }
 
