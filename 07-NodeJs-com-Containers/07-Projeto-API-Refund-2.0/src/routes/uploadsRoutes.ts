a75c00dc9ed46a6dd6e2b71afd5abfbc935bd559
Davi <davi.rferreira@gmail.com>
2025-10-23 11:14:16 -0300

    API-Refund-2.0 - Rota para exibição de arquivos salvos

--- 07-NodeJs-com-Containers/07-Projeto-API-Refund-2.0/src/routes/uploadsRoutes.ts
+++ 07-NodeJs-com-Containers/07-Projeto-API-Refund-2.0/src/routes/uploadsRoutes.ts
@@ -1,20 +1,26 @@
-import { Router } from 'express'
+import express, { Router } from 'express'
 import multer from 'multer'
 
 import { UploadsController } from '@/controllers/uploadsController.js'
 import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization.js'
-import * as uploadConfig from '@/configs/upload.js'
+import * as uploadConfigs from '@/configs/upload.js'
 
 const uploadsRoutes = Router()
 const uploadsController = new UploadsController()
 
-const upload = multer(uploadConfig.MULTER)
+const upload = multer(uploadConfigs.MULTER)
 
 uploadsRoutes.post(
   '/',
   verifyUserAuthorization(['employee']),
   upload.single('file'),
   uploadsController.create
+)
+
+uploadsRoutes.use(
+  '/',
+  verifyUserAuthorization(['manager']),
+  express.static(uploadConfigs.UPLOADS_FOLDER)
 )
 
 export { uploadsRoutes }
