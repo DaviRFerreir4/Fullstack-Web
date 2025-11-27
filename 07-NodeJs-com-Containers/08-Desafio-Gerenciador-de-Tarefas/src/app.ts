b2db96b2229cac6e0527c6ce985fa4339cf79567
Davi <davi.rferreira@gmail.com>
2025-11-27 08:22:32 -0300

    Gerenciador-de-Tarefas - Função para tratamento de errors e configurações do JWT

--- 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/app.ts
+++ 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 import 'express-async-errors'
 import { prisma } from './database/prisma'
+import { errorHandling } from './middlewares/error-handling'
+import { AppError } from '../utils/app-error'
 
 const app = express()
+
+app.use(errorHandling)
 
 export { app }
