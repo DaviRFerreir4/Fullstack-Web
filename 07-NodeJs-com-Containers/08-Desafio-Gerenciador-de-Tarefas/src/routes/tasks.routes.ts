b29c616c82346f8364556ccb3da2e630906925aa
Davi <davi.rferreira@gmail.com>
2025-11-28 11:47:32 -0300

    Gerenciador-de-Tarefas - Reduzindo acesso a rota de atualização de dados das tarefas somente para admins

--- 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/routes/tasks.routes.ts
+++ 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/routes/tasks.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express'
 
 import { TasksController } from '../controllers/tasks.controller'
+import { verifyUserAuthorization } from '../middlewares/verify-user-authorization'
 
 const tasksRoutes = Router()
 const tasksController = new TasksController()
 
 tasksRoutes.get('/', tasksController.index)
 tasksRoutes.post('/', tasksController.create)
-tasksRoutes.put('/:id', tasksController.update)
+tasksRoutes.put(
+  '/:id',
+  verifyUserAuthorization(['admin']),
+  tasksController.update
+)
 tasksRoutes.patch('/:id', tasksController.patch)
 tasksRoutes.delete('/:id', tasksController.remove)
 
