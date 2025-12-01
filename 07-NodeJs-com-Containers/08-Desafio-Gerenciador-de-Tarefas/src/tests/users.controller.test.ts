18ca0fcea5d5854cd5ab467665a41ad922a57818
Davi <davi.rferreira@gmail.com>
2025-12-01 10:13:13 -0300

    Gerenciador-de-Tarefas - Testando erro cadastrando usuario com email ja existente com Jest

--- 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/tests/users.controller.test.ts
+++ 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/tests/users.controller.test.ts
@@ -6,6 +6,13 @@ import { prisma } from '../database/prisma'
 describe('UsersController', () => {
   let token: string
 
+  const userData = {
+    name: 'Test User',
+    email: 'testuser@example.com',
+    password: '12345678',
+    confirm_password: '12345678',
+  }
+
   beforeAll(async () => {
     const sessionResponse = await request(app).post('/sessions').send({
       email: 'davi@email.com',
@@ -29,13 +36,22 @@ describe('UsersController', () => {
     const response = await request(app)
       .post('/users')
       .send({
-        name: 'Test User',
-        email: 'testuser@example.com',
-        password: '12345678',
-        confirm_password: '12345678',
+        ...userData,
       })
       .auth(token, { type: 'bearer' })
 
     expect(response.statusCode).toBe(201)
+  })
+
+  it('should throw an error if user with same email already exists', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        ...userData,
+      })
+      .auth(token, { type: 'bearer' })
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toHaveProperty('message')
   })
 })
