97473f6375770c62220b660d971ff65083641561
Davi <davi.rferreira@gmail.com>
2025-12-01 10:39:39 -0300

    Gerenciador-de-Tarefas - Testando rota de criação de times com Jest

---
+++ 07-NodeJs-com-Containers/08-Desafio-Gerenciador-de-Tarefas/src/tests/teams.controller.test.ts
@@ -0,0 +1,43 @@
+import request from 'supertest'
+
+import { app } from '../app'
+import { prisma } from '../database/prisma'
+
+describe('TeamsController', () => {
+  let token: string
+
+  const teamData = {
+    name: 'Test Team',
+    description: 'Team used to test the /teams endpoint on the API',
+  }
+
+  beforeAll(async () => {
+    const sessionResponse = await request(app).post('/sessions').send({
+      email: 'davi@email.com',
+      password: '12345678',
+    })
+
+    token = sessionResponse.body.token
+  })
+
+  afterAll(async () => {
+    const team = await prisma.team.findFirst({ where: { name: teamData.name } })
+
+    if (!team) {
+      return
+    }
+
+    await prisma.team.delete({ where: { id: team.id } })
+  })
+
+  it('should create a new team successfully', async () => {
+    const response = await request(app)
+      .post('/teams')
+      .send({
+        ...teamData,
+      })
+      .auth(token, { type: 'bearer' })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
