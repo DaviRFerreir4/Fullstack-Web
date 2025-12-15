70fa8389a810bbdb3823ba5fc385a6087f84f3ae
Davi <davi.rferreira@gmail.com>
2025-12-15 09:41:54 -0300

    HelpDesk-Backend - Testando criação de usuário técnico na rota de usuários com Jest

--- 10-Desafio-HelpDesk/Backend/src/tests/users.test.ts
+++ 10-Desafio-HelpDesk/Backend/src/tests/users.test.ts
@@ -8,7 +8,7 @@ import { email } from 'zod'
 describe('UsersController', () => {
   let adminToken: string
   let userToken: string
-  let userId: string
+  let usersId: string[] = []
 
   beforeAll(async () => {
     const adminResponse = await request(app).post('/sessions').send({
@@ -19,15 +19,49 @@ describe('UsersController', () => {
     adminToken = adminResponse.body.token
   })
 
+  afterAll(async () => {
+    for (const id of usersId) {
+      const user = await prisma.user.findUnique({ where: { id } })
+
+      if (user && user.role === 'technician') {
+        await prisma.openingHour.delete({ where: { userId: id } })
+      }
+
+      await prisma.user.delete({ where: { id } })
+    }
+  })
+
   it('should create a new user successfully', async () => {
     const userResponse = await request(app).post('/users').send(userData)
 
-    userId = userResponse.body.user.id
-
     expect(userResponse.statusCode).toBe(201)
+
+    usersId.push(userResponse.body.user.id)
+
     expect(userResponse.body).toHaveProperty('user')
     expect(userResponse.body.user).toEqual(
       expect.objectContaining({ name: userData.name, email: userData.email })
+    )
+  })
+
+  it('should create a technician user', async () => {
+    const userResponse = await request(app)
+      .post('/users/technician')
+      .send({
+        ...userData,
+        email: 'test.user2@email.com',
+        role: 'technician',
+        availableHours: [8, 9, 10, 12, 13, 14, 15, 16],
+      })
+      .auth(adminToken, { type: 'bearer' })
+
+    expect(userResponse.statusCode).toBe(201)
+
+    usersId.push(userResponse.body.user.id)
+
+    expect(userResponse.body).toHaveProperty('user')
+    expect(userResponse.body.user).toEqual(
+      expect.objectContaining({ role: 'technician' })
     )
   })
 
@@ -57,7 +91,7 @@ describe('UsersController', () => {
     userToken = sessionResponse.body.token
 
     const userResponse = await request(app)
-      .get(`/users/${userId}`)
+      .get(`/users/${usersId[0]}`)
       .auth(userToken, { type: 'bearer' })
 
     expect(userResponse.statusCode).toBe(200)
@@ -69,12 +103,12 @@ describe('UsersController', () => {
 
   it('should update information about the user', async () => {
     const userResponse = await request(app)
-      .put(`/users/${userId}`)
+      .put(`/users/${usersId[0]}`)
       .send({
         name: 'Test User Update',
         email: 'roberto@email.com',
         password: 'TestUpdatePassword2.',
-        confirm_password: 'TestUpdatePassword2.',
+        confirmPassword: 'TestUpdatePassword2.',
       })
       .auth(userToken, { type: 'bearer' })
 
@@ -86,8 +120,10 @@ describe('UsersController', () => {
 
   it('should delete the user', async () => {
     const userResponse = await request(app)
-      .delete(`/users/${userId}`)
+      .delete(`/users/${usersId[0]}`)
       .auth(userToken, { type: 'bearer' })
+
+    usersId.splice(0, 1)
 
     expect(userResponse.statusCode).toBe(200)
   })
--- 10-Desafio-HelpDesk/Backend/src/tests/utils/requestData.ts
+++ 10-Desafio-HelpDesk/Backend/src/tests/utils/requestData.ts
@@ -2,5 +2,5 @@ export const userData = {
   name: 'Test User',
   email: 'test.user@email.com',
   password: 'TestPassword123.',
-  confirm_password: 'TestPassword123.',
+  confirmPassword: 'TestPassword123.',
 }
