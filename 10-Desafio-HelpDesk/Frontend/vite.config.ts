e375f0c46be849fffe1b5c3889fe4b6cfbd1ec61
Davi <davi.rferreira@gmail.com>
2025-12-29 11:35:24 -0300

    HelpDesk-Frontend - Configurando estilos (tailwind) e subindo assets para o projeto

--- 10-Desafio-HelpDesk/Frontend/vite.config.ts
+++ 10-Desafio-HelpDesk/Frontend/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss()],
   server: {
-    // configurações importantes para docker
+    // configurações importantes para docker utilizando volumes para desenvolvimento
     host: true,
     port: 5173,
     watch: {
