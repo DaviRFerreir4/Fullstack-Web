b267d7bf1bfd6c91a5d5dc967dc1bd0c0f8b927f
Davi <davi.rferreira@gmail.com>
2025-12-29 14:34:41 -0300

    HelpDesk-Frontend - Pagina de login feita (mobile)

--- 10-Desafio-HelpDesk/Frontend/src/pages/SignIn.tsx
+++ 10-Desafio-HelpDesk/Frontend/src/pages/SignIn.tsx
@@ -1,5 +1,40 @@
+import { Input } from '../components/form/Input'
 import { Button } from '../components/form/Button'
 
 export function SignIn() {
-  return <Button text="Test" />
+  return (
+    <div className="w-full">
+      <form className="mt-6 p-6 border border-gray-500 rounded-[0.625rem] grid gap-8">
+        <div>
+          <h2 className="text-lg text-gray-200 font-bold">Acesse o portal</h2>
+          <p className="text-xs text-gray-300">
+            Entre usando seu e-mail e senha cadastrados
+          </p>
+        </div>
+        <div className="grid gap-4">
+          <Input
+            label="E-mail"
+            id="email"
+            type="text"
+            placeholder="exemplo@mail.com"
+          />
+          <Input
+            label="Senha"
+            id="password"
+            type="password"
+            placeholder="Digite sua senha"
+            error={true}
+          />
+        </div>
+        <Button text="Entrar" />
+      </form>
+      <div className="my-3 p-6 border border-gray-500 rounded-[0.625rem] grid gap-5">
+        <div>
+          <h3 className="text-gray-200 font-bold">Ainda não tem uma conta?</h3>
+          <p className="text-xs text-gray-300">Cadastre agora mesmo</p>
+        </div>
+        <Button text="Criar conta" variant="secondary" />
+      </div>
+    </div>
+  )
 }
