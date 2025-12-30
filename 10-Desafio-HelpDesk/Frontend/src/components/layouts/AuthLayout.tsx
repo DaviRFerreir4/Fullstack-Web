1d53f96e52c0e5d2bd4d75f148183fd1e7f959f3
Davi <davi.rferreira@gmail.com>
2025-12-30 08:29:05 -0300

    HelpDesk-Frontend - Responsividade do layout para o desktop feita

--- 10-Desafio-HelpDesk/Frontend/src/components/layouts/AuthLayout.tsx
+++ 10-Desafio-HelpDesk/Frontend/src/components/layouts/AuthLayout.tsx
@@ -5,15 +5,16 @@ import bgImage from '../../assets/login-background.png'
 
 export function AuthLayout() {
   return (
-    <div className="h-screen">
-      <img src={bgImage} alt="" className="absolute -z-10 inset-0" />
-      <main className="h-screen mt-8 px-6 py-8 rounded-[1.25rem] flex flex-col items-center bg-gray-600">
-        <div className="flex items-center gap-3">
-          <img src={logo} className="w-10 h-10" />
-          <h1 className="text-blue-dark text-xl">HelpDesk</h1>
-        </div>
-        <Outlet />
-      </main>
+    <div style={{ backgroundImage: `url(${bgImage})` }} className="flex">
+      <div className="lg:w-[50%] ml-auto">
+        <main className="min-h-screen mt-8 lg:mt-3 px-6 pt-12 lg:px-36 py-8 rounded-t-[1.25rem] flex flex-col items-center bg-gray-600">
+          <div className="flex items-center gap-3">
+            <img src={logo} className="w-10 h-10" />
+            <h1 className="text-blue-dark text-xl">HelpDesk</h1>
+          </div>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
