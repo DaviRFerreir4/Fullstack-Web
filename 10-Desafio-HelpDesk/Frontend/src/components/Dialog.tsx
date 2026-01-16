d93ce9b7634a467266dff739bf1157d0f4c1007a
Davi <davi.rferreira@gmail.com>
2026-01-16 10:21:56 -0300

    HelpDesk-Frontend - Última mudança antes da integração (form como wrapper do Dialog quando houver criação ou edição de dados)

--- 10-Desafio-HelpDesk/Frontend/src/components/Dialog.tsx
+++ 10-Desafio-HelpDesk/Frontend/src/components/Dialog.tsx
@@ -43,6 +43,9 @@ export function Dialog({
   ...rest
 }: Props) {
   let buttonText = ''
+  const Wrapper = ['create', 'edit', 'changePassword'].includes(action)
+    ? 'form'
+    : 'div'
 
   if (action === 'create' || action === 'edit' || action === 'changePassword')
     buttonText = 'Salvar'
@@ -66,51 +69,64 @@ export function Dialog({
       className="py-5 rounded-[0.625rem] w-[min(27.5rem,92%)] m-auto backdrop:bg-black/50 backdrop:backdrop-blur-[2px]"
       {...rest}
     >
-      <div className="px-7 pb-5 flex justify-between items-center gap-3">
-        {action === 'changePassword' && (
-          <BackIcon
-            className="w-4.5 h-4.5 text-gray-300 cursor-pointer"
-            onClick={backAction}
-          />
-        )}
-        <h1 className={`font-bold ${action === 'changePassword' && 'flex-1'}`}>
-          {title}
-        </h1>
-        <CloseIcon
-          className="text-gray-300 w-4.5 h-4.5 cursor-pointer"
-          onClick={closeDialog}
-        />
-      </div>
-      <div
-        className={`mb-6 py-7 border-y border-gray-500 ${
-          useSamePadding && 'px-7'
-        }`}
-      >
-        {action === 'success' ? (
-          <div className="grid justify-center justify-items-center gap-4">
-            <SuccessIcon className="w-12 h-12 text-feedback-done/70" />
-            <span>Ação realizada com sucesso!</span>
-          </div>
-        ) : action === 'failure' ? (
-          <div className="grid justify-center justify-items-center gap-4">
-            <FailureIcon className="w-12 h-12 text-feedback-danger/70" />
-            <div>
-              <p className="mb-1">Erro ao realizar a ação requisitada.</p>
-              <p>Por favor, tente novamente mais tarde.</p>
-            </div>
-          </div>
-        ) : (
-          children
-        )}
-      </div>
-      <div className="px-7">
-        <div className={cancelAction ? `flex gap-2` : undefined}>
-          {cancelAction && (
-            <Button text="Cancelar" variant="secondary" onClick={closeDialog} />
+      <Wrapper onSubmit={Wrapper === 'form' ? handleAction : undefined}>
+        <div className="px-7 pb-5 flex justify-between items-center gap-3">
+          {action === 'changePassword' && (
+            <BackIcon
+              className="w-4.5 h-4.5 text-gray-300 cursor-pointer"
+              onClick={backAction}
+            />
           )}
-          <Button text={buttonText} onClick={handleAction} />
+          <h1
+            className={`font-bold ${action === 'changePassword' && 'flex-1'}`}
+          >
+            {title}
+          </h1>
+          <CloseIcon
+            className="text-gray-300 w-4.5 h-4.5 cursor-pointer"
+            onClick={closeDialog}
+          />
         </div>
-      </div>
+        <div
+          className={`mb-6 py-7 border-y border-gray-500 ${
+            useSamePadding && 'px-7'
+          }`}
+        >
+          {action === 'success' ? (
+            <div className="grid justify-center justify-items-center gap-4">
+              <SuccessIcon className="w-12 h-12 text-feedback-done/70" />
+              <span>Ação realizada com sucesso!</span>
+            </div>
+          ) : action === 'failure' ? (
+            <div className="grid justify-center justify-items-center gap-4">
+              <FailureIcon className="w-12 h-12 text-feedback-danger/70" />
+              <div>
+                <p className="mb-1">Erro ao realizar a ação requisitada.</p>
+                <p>Por favor, tente novamente mais tarde.</p>
+              </div>
+            </div>
+          ) : (
+            children
+          )}
+        </div>
+        <div className="px-7">
+          <div className={cancelAction ? `flex gap-2` : undefined}>
+            {cancelAction && (
+              <Button
+                text="Cancelar"
+                type="button"
+                variant="secondary"
+                onClick={closeDialog}
+              />
+            )}
+            <Button
+              text={buttonText}
+              type={Wrapper === 'div' ? 'button' : 'submit'}
+              onClick={Wrapper === 'div' ? handleAction : undefined}
+            />
+          </div>
+        </div>
+      </Wrapper>
     </dialog>
   )
 }
