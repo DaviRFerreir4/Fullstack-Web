b714fa036b3a27a2a11ad231182f5c06e5a4c3bb
Davi <davi.rferreira@gmail.com>
2026-01-16 10:55:14 -0300

    HelpDesk-Frontend - docker-compose para rodar a aplicação toda em desenvolvimento

--- 10-Desafio-HelpDesk/Frontend/vite.config.ts
+++ 10-Desafio-HelpDesk/Frontend/vite.config.ts
@@ -5,12 +5,12 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svgr()],
-  server: {
+  /*server: {
     // configurações importantes para docker utilizando volumes para desenvolvimento
     host: true,
     port: 5173,
     watch: {
       usePolling: true, // faz com que o o subsistema de linux no windows (docker) consiga verificar alterações nos arquivos do windows, coisa que ele não faz normalmente (usePolling aparentemente consome mais CPU. A melhor alternativa seria alterar os arquivos no próprio subsistema de linux, ou seja, o container do docker, isso pelo que eu entendi)
     },
-  },
+  },*/
 })
