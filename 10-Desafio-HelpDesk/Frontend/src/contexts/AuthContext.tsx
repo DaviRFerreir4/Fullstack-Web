94803e663cb2037fe7ab5b204becf4495b22e7e1
Davi <davi.rferreira@gmail.com>
2026-01-22 13:47:03 -0300

    HelpDesk-Frontend - Criando e utilizando um contexto de autenticação

---
+++ 10-Desafio-HelpDesk/Frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,7 @@
+import { createContext, type ReactNode } from 'react'
+
+export const AuthContext = createContext({})
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
+}
--- 10-Desafio-HelpDesk/Frontend/src/routes/index.tsx
+++ 10-Desafio-HelpDesk/Frontend/src/routes/index.tsx
@@ -6,11 +6,16 @@ import { TechnicianRoutes } from './TechnicianRoutes'
 import { ClientRoutes } from './ClientRoutes'
 
 import { users } from '../data/users'
+import { useAuth } from '../hooks/useAuth'
 
 const user = users.find((user) => user.id === localStorage.getItem('userid'))
 const role = user?.role
 
 export function Routes() {
+  const authContext = useAuth()
+
+  console.log(authContext)
+
   return (
     <BrowserRouter>
       {role === 'client' ? (
